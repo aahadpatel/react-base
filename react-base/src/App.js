759fb0071217d74d1ecc7522bcc00d540c50de95
Aahad patel <aahad.patel786@gmail.com>
2020-11-29 22:46:36 -0600

    adding react-base

---
+++ react-base/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import CounterExample from './Components/CounterExample';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Views/Home';
+import About from './Views/About';
+
+function App() {
+  return (
+    <div>
+      
+      {/* <CounterExample /> */}
+      <Router>
+        <Header />
+
+        <div className="p-3">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
